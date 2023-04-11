70ec204433e3993bb933582eca631cf3b75688cd
ariel <ariel1434@gmail.com>
2023-04-11 19:45:46 +0300

    Switch to ip-api.com for fetching public IP address
    - Replace IPify API with ip-api.com due to connection issues
    - Update endpoint and response handling accordingly

--- project-1/server.mjs
+++ project-1/server.mjs
@@ -7,10 +7,22 @@ const app = express();
 app.use(cors());
 
 app.get('/api/ip', async function (req, res) {
-  const response = await fetch('https://api.ipify.org?format=json');
-  const data = await response.json();
-  const ip = data.ip;
-  res.send({ ip: ip });
+  try {
+    const response = await fetch('http://ip-api.com/json/?fields=status,query');
+    const data = await response.json();
+
+    if (data.status === 'fail') {
+      console.error('Error fetching IP:', data);
+      res.status(500).send({ error: 'Failed to fetch IP address' });
+      return;
+    }
+
+    const ip = data.query;
+    res.send({ ip: ip });
+  } catch (error) {
+    console.error('Error fetching IP:', error);
+    res.status(500).send({ error: 'Failed to fetch IP address' });
+  }
 });
 
 app.listen(3001, () => {
